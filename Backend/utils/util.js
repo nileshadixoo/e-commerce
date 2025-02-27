f96ba78ff5a19b75c126a14aa4ddcd6848e2abe7
nileshadixoo <nilesh.chouhan@adixoo.com>
2025-02-27 10:26:10 +0530

    Backend completed

---
+++ Backend/utils/util.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken'
+
+export const createToken = async (user_email) => {
+  try {
+    const payload = { user: user_email };
+    const token = await jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    return token;
+  } catch (error) {
+    throw new error(error);
+  }
+};
+
+export const verifyToken =  (token) => {
+    try {
+        var decoded = jwt.verify(token, process.env.JWT_SECRET);
+        return decoded;
+    } catch (error) {
+        console.log(error)
+    }
+};
